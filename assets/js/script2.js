d7ec691e32fe8c06d571e1c16ad33a6c28abb377
Javier Vergara <ja.vergarac96@gmail.com>
2024-02-15 00:04:48 -0300

    desarrollo ejercicio2

---
+++ assets/js/script2.js
@@ -0,0 +1,38 @@
+let rojo = document.getElementById('btn-1').style.backgroundColor;
+let naranja = document.getElementById('btn-2').style.backgroundColor;
+let amarillo = document.getElementById('btn-3').style.backgroundColor;
+let verde = document.getElementById('btn-4').style.backgroundColor;
+let celeste = document.getElementById('btn-5').style.backgroundColor;
+let violeta = document.getElementById('btn-6').style.backgroundColor;
+
+function colorRojo(event){
+    event.preventDefault();
+    document.getElementById('caja').style.backgroundColor = rojo;
+}
+function colorNaranja(event){
+    event.preventDefault();
+    document.getElementById('caja').style.backgroundColor = naranja;
+}
+function colorAmarillo(event){
+    event.preventDefault();
+    document.getElementById('caja').style.backgroundColor = amarillo;
+}
+function colorVerde(event){
+    event.preventDefault();
+    document.getElementById('caja').style.backgroundColor = verde;
+}
+function colorCeleste(event){
+    event.preventDefault();
+    document.getElementById('caja').style.backgroundColor = celeste;
+}
+function colorVioleta(event){
+    event.preventDefault();
+    document.getElementById('caja').style.backgroundColor = violeta;
+}
+
+document.getElementById("btn-1").addEventListener("click", colorRojo);
+document.getElementById("btn-2").addEventListener("click", colorNaranja);
+document.getElementById("btn-3").addEventListener("click", colorAmarillo);
+document.getElementById("btn-4").addEventListener("click", colorVerde);
+document.getElementById("btn-5").addEventListener("click", colorCeleste);
+document.getElementById("btn-6").addEventListener("click", colorVioleta);
