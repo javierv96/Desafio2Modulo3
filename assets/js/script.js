075c0635ef94953c646a31ba1d3693d04935fac3
Javier Vergara <ja.vergarac96@gmail.com>
2024-02-15 00:04:11 -0300

    desarrollo ejercicio1

---
+++ assets/js/script.js
@@ -0,0 +1,60 @@
+let expresionRegular = /^[A-Za-z]+$/;
+
+function login(event) {
+    event.preventDefault();
+    let nombre = document.getElementById('nombre').value;
+    let asunto = document.getElementById('asunto').value;
+    let mensaje = document.getElementById('mensaje').value;
+
+    validacionNombre(nombre);
+    validacionAsunto(asunto);
+    validacionMensaje(mensaje);
+
+    if(expresionRegular.test(nombre) && expresionRegular.test(asunto) && expresionRegular.test(mensaje)){
+        document.querySelector(".resultado").innerHTML = "Mensaje enviado con éxito !!!";
+    }
+
+}
+
+function validacionNombre(nombre) {
+    if (nombre !== "") {
+        if (expresionRegular.test(nombre)) {
+            document.querySelector(".errorNombre").innerHTML = "";
+        } else {
+            document.querySelector(".errorNombre").innerHTML = "Ingrese un nombre valido.";
+            alert('El formulario solo acepta caracteres de la "a" hasta la "z". Favor ingrese un nombre valido')
+        }
+    } else {
+        document.querySelector(".errorNombre").innerHTML = "El nombre es requerido.";
+    }
+}
+
+function validacionAsunto(asunto) {
+    if (asunto !== "") {
+        if (expresionRegular.test(asunto)) {
+            document.querySelector(".errorAsunto").innerHTML = "";
+        } else {
+            document.querySelector(".errorAsunto").innerHTML = "Ingrese un asunto valido.";
+            alert('El formulario solo acepta caracteres de la "a" hasta la "z". Favor ingrese un asunto valido')
+        }
+    } else {
+        document.querySelector(".errorAsunto").innerHTML = "El asunto es requerido.";
+    }
+}
+
+function validacionMensaje(mensaje) {
+    if (mensaje !== "") {
+        if (expresionRegular.test(mensaje)) {
+            document.querySelector(".errorMensaje").innerHTML = "";
+        } else {
+            document.querySelector(".errorMensaje").innerHTML = "Ingrese un mensaje valido.";
+            alert('El formulario solo acepta caracteres de la "a" hasta la "z". Favor ingrese un mensaje valido')
+        }
+    } else {
+        document.querySelector(".errorMensaje").innerHTML = "El mensaje es requerido.";
+    }
+}
+
+let form = document.getElementById("formulario");
+
+form.addEventListener("submit", login);
